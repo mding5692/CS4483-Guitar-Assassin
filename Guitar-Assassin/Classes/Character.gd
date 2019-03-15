70548045cf16b2c308045fe233632807f1ce1c4e
Michael Ding <m.ding5692@gmail.com>
2019-03-15 15:27:37 -0400

    Base character class init

---
+++ Guitar-Assassin/Classes/Character.gd
@@ -0,0 +1,21 @@
+extends Node
+
+class_name Character 
+
+var hp = 100
+var speed = 5
+var xy_coords = Vector2D(0,0)
+
+func _ready():
+	# Called when the node is added to the scene for the first time.
+	# Initialization here
+	pass
+
+func use_ability():
+	
+	
+
+#func _process(delta):
+#	# Called every frame. Delta is time since last frame.
+#	# Update game logic here.
+#	pass
