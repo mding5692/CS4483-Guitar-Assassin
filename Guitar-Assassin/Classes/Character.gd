746763390b9698cda8520052a09aa57745d6dbce
Michael Ding <m.ding5692@gmail.com>
2019-03-29 14:06:20 -0400

    Enemy movement randomized

--- Guitar-Assassin/Classes/Character.gd
+++ Guitar-Assassin/Classes/Character.gd
@@ -11,31 +11,15 @@ var direction = 'right'
 const HIT_TIMER_MAX = 25
 var hit_timer = HIT_TIMER_MAX
 var collision_state = false
-#var moves = { 'right': Vector2(1,0), 'left': Vector2(-1,0), 'up': Vector2(0, -1), 'down': Vector2(0,1) }
 var raycasts = { 'right': 'RayCastRight', 'left': 'RayCastLeft', 'up': 'RayCastUp', 'down': 'RayCastDown' }
 
 # Character-specific variables:
 var hp = 100
 const SPEED = 0
 
-
 func use_ability():
 	pass
 
-"""func move(dir):
-	direction = dir
-	if get_node(raycasts[direction]).is_colliding():
-		return
-	can_move = false
-	$AnimationPlayer.play(direction)
-	$MoveTween.interpolate_property(self, 'position', position, position + moves[direction] * tile_size, 0.1, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-	$MoveTween.start()
-	return true
-
-func _on_MoveTween_tween_completed(object, key):
-	can_move = true
-	pass # replace with function body
-"""
 func character_movement():
 	var motion
 	if hit_timer == HIT_TIMER_MAX:
--- Guitar-Assassin/Classes/Enemy.gd
+++ Guitar-Assassin/Classes/Enemy.gd
@@ -1,15 +1,33 @@
 extends "res://Classes/Character.gd"
 
-const SPEED = 40
-var enemy_moves = [Vector2(1,0), Vector2(-1,0), Vector2(0, -1), Vector2(0,1)]
+const SPEED = 30
+const TYPE = "ENEMY"
+var enemy_moves = {'up': Vector2(0,-1), 'right': Vector2(1,0), 'left': Vector2(-1,0), 'down': Vector2(0,1)}
+const animation = "walk"
+
+# For handling how much time it talks to walk
+var movetimer_length = 15
+var movetimer = 0
 
 func _ready():
 	randomize()
 
 func _process(delta):
-	enemy_movement()
 	character_movement()
-	sprite_direction()
+	if movetimer > 0:
+		movetimer -= 1
+	elif movetimer == 0:
+		change_enemy_direction()
+		movetimer = movetimer_length
+	
 
-func enemy_movement():
-	move_direction = enemy_moves[randi() % 4]
+func change_enemy_direction():
+	var randNum = randi() % 4
+	direction = enemy_moves.keys()[randNum]
+	move_direction = enemy_moves.values()[randNum]
+	enemy_animation()
+	
+func enemy_animation():
+	var newanim = str(animation, direction)
+	if $anim.current_animation != newanim:
+		$anim.play(newanim)
