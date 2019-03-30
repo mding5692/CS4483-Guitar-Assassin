70f363f3e7441e1d9f173a8e6413c3f327c75ad3
Stepan Khomyn <skhomyn@uwo.ca>
2019-03-30 17:58:10 -0400

    Crash bug fixed

--- Guitar-Assassin/DialogBox.gd
+++ Guitar-Assassin/DialogBox.gd
@@ -1,9 +1,6 @@
 # DialogBox.gd
 extends RichTextLabel
 
-# class member variables go here, for example:
-# var a = 2
-# var b =  "textvar"
 var dialog = ["Hello Guitar Assassin, welcome!", "Are you ready to begin your adventure?"]
 var page = 0
 
@@ -12,17 +9,22 @@ func _ready():
 	set_visible_characters(0) 		# hide text at start
 	set_process_input(true) 		# enable user input
 
-	
+
+# TODO
+# Remove dialog box after last page
 func _input(event):
-	if event is InputEventKey and event.get_scancode() == KEY_SPACE:
-		if event.is_pressed():
-			if get_visible_characters() > get_total_character_count():
-				if page < dialog.size()-1:
-					page += 1
+	if event is InputEventKey and event.get_scancode() == KEY_SPACE: 	# scan for input
+		if event.is_pressed():											# if space key is pressed
+			if get_visible_characters() > get_total_character_count():	# if all text for current page printed
+				if page < dialog.size()-1:								# if not last page of dialog
+					page += 1											# turn page
 					set_bbcode(dialog[page])
 					set_visible_characters(0)
-			else:
-				set_visible_characters(get_total_character_count())
+#				else:					
+#					$Dialog_Box.visible = not $Dialog_Box.visible
+
+			else:														# 
+				set_visible_characters(get_total_character_count())		
 
 
 
