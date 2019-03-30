d89d3e6631ac589e89389cdcc7407c39ad344e89
Michael Ding <m.ding5692@gmail.com>
2019-03-30 17:25:49 -0400

    Enemy follows player if within certain radius

--- Guitar-Assassin/Classes/Enemy.gd
+++ Guitar-Assassin/Classes/Enemy.gd
@@ -16,7 +16,6 @@ var movetimer = 0
 func _ready():
 	speed = 30
 	randomize()
-#	set_up_visibility_radius()
 
 func _process(delta):
 	character_movement()
@@ -29,15 +28,6 @@ func _process(delta):
 		elif movetimer == 0:
 			change_enemy_direction()
 			movetimer = movetimer_length
-
-#func set_up_visibility_radius():
-#	var shape = CircleShape2D.new()
-#	shape.radius = detect_radius
-#	$Visibility/CollisionShape2D.shape = shape
-	
-#	#	Sets up signals for when enter area2D
-#	get_node("Visibility").connect("body_entered", self, "on_Visibility_body_entered")
-#	get_node("Visibility").connect("body_exited", self, "on_Visibility_body_exited")
 
 func change_to_player_direction():
 	if move_direction.x < 0:
@@ -64,9 +54,9 @@ func enemy_animation():
 		$anim.play(newanim)
 
 func _on_Visibility_body_entered(body):
-	var player_to_enemy_distance = (player_target.global_position - self.global_position).normalized()
-#		sees_player = true
-
+	if body.get_name() == "player":
+		sees_player = true
 
 func _on_Visibility_body_exited(body):
-	sees_player = false
+	if body.get_name() == "player":
+		sees_player = false
