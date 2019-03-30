004c2a292a2ca1ed0a10b690630e20102a6a5053
Geraldine Leung <geraldine.leung2@gmail.com>
2019-03-30 14:52:53 -0400

    destroy bullet added

--- Guitar-Assassin/Classes/Bullets/blue_bullet.gd
+++ Guitar-Assassin/Classes/Bullets/blue_bullet.gd
@@ -1,7 +1,18 @@
 extends "res://Classes/Bullets/bullet.gd"
 
+const bullet_speed = 30
+var bullet_bounce = 0
+
 func _ready():
 	pass
 
-func destroy_bullet():
-	print("bullet")
+func _physics_process(delta):
+	destroy_bullet(delta)
+
+func destroy_bullet(delta):
+	var collision = move_and_collide(bullet_speed * delta * Vector2(0,0).normalized())
+	if collision:
+		if collision.collider.get_name() == "TileMap":
+			bullet_bounce += 1
+			if bullet_bounce == 2:
+				queue_free()
