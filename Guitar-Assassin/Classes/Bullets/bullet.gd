24474ca5fb24c7c973b1cfcd678208342fb7eeab
Geraldine Leung <geraldine.leung2@gmail.com>
2019-03-30 15:27:37 -0400

    bounce and destroy bullet

--- Guitar-Assassin/Classes/Bullets/bullet.gd
+++ Guitar-Assassin/Classes/Bullets/bullet.gd
@@ -1,8 +1,9 @@
 #code from http://kidscancode.org/blog/2018/02/godot3_kinematic2d/
 extends KinematicBody2D
 
-var speed = 750
+var speed = 350
 var velocity = Vector2()
+var bullet_bounce = 0
 
 func bullet_start_position(pos, dir):
 	rotation = dir
@@ -13,6 +14,6 @@ func _physics_process(delta):
 	var collision = move_and_collide(velocity * delta)
 	if collision:
 		velocity = velocity.bounce(collision.normal)
-
-func _on_VisibilityNotifier2D_screen_exited():
-	queue_free()
+		bullet_bounce += 1
+		if bullet_bounce == 2:
+			queue_free()
--- Guitar-Assassin/Classes/Bullets/yellow_bullet.gd
+++ Guitar-Assassin/Classes/Bullets/yellow_bullet.gd
@@ -1,4 +1,6 @@
 extends "res://Classes/Bullets/bullet.gd"
 
+#var bullet_bounce = 0
+
 func _ready():
 	pass
