1d5a111a9727e7aae8afb0580ca923b7e200dbc0
mmoriya <mmoriya.uwo.ca>
2019-03-31 18:20:06 -0400

    Updates fade transition

--- Guitar-Assassin/fade_in/fade_in.gd
+++ Guitar-Assassin/fade_in/fade_in.gd
@@ -3,7 +3,7 @@ extends ColorRect
 signal fade_finished
 
 func fade_in():
-	$AnimationPlayer.play("fade_in")
+	$Fade.play("fade_in")
 
 func _on_FadeIn_animation_finished(anim_name):
 	emit_signal("fade_finished")
