887b626e56b246ed10be67949bc52e819241201e
Michael Ding <m.ding5692@gmail.com>
2019-03-31 18:31:43 -0400

    Title Screen moves to game

--- Guitar-Assassin/Title_Screen/Title.gd
+++ Guitar-Assassin/Title_Screen/Title.gd
@@ -12,8 +12,9 @@ func _on_Button_pressed(scene_to_load):
 	$FadeIn.show()
 	$FadeIn.fade_in() 
 
-func _on_FadeIn_fade_finished():
-	get_tree().change_scene(scene_path_to_load)
-
 func _on_QuitButton_pressed():
 	get_tree().quit()
+
+
+func _on_FadeIn_fade_finished():
+	get_tree().change_scene(scene_path_to_load)
--- Guitar-Assassin/fade_in/fade_in.gd
+++ Guitar-Assassin/fade_in/fade_in.gd
@@ -5,5 +5,5 @@ signal fade_finished
 func fade_in():
 	$Fade.play("fade_in")
 
-func _on_FadeIn_animation_finished(anim_name):
+func _on_Fade_animation_finished(anim_name):
 	emit_signal("fade_finished")
