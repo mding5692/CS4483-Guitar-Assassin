96a71e28f28fc68bd7f162be6a58820bf4f93c24
Stepan Khomyn <skhomyn@uwo.ca>
2019-03-31 16:42:02 -0400

    Read dialog only once

--- Guitar-Assassin/Classes/NPC.gd
+++ Guitar-Assassin/Classes/NPC.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
 onready var dialog_box = get_parent().get_node("player").get_node("Camera2D2").get_node("Dialog_Box/Main Text")
+var dialog_started = false
 
 func _on_Visibility_body_entered(body):
-	if body.get_name() == "player":
+	if body.get_name() == "player" && dialog_started == false:
 		dialog_box.start_dialog()
+		dialog_started = true
+		
