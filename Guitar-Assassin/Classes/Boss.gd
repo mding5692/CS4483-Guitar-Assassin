e4e91f48a453c0b2646cbb95dd4743139c94641c
Michael Ding <m.ding5692@gmail.com>
2019-04-02 16:33:24 -0400

    Gives Boss an axe

--- Guitar-Assassin/Classes/Boss.gd
+++ Guitar-Assassin/Classes/Boss.gd
@@ -4,4 +4,10 @@ func _ready():
 	hp = 50
 	type = "Boss"
 	speed = 30
-	
+	$Axe/Sprite.visible = false
+
+func use_weapon():
+	print("B")
+	$Axe/Sprite.visible = true
+	$Axe/anim.play("swingup")
+	print("C")
